14e0604eb5e7ed6dcba86cd4e8ecd94411450393
AnnaWronowska <103896687+AnnaWronowska@users.noreply.github.com>
2022-04-19 22:38:45 +0200

    Update find_leader.py

--- DataSets'Properties/find_leader.py
+++ DataSets'Properties/find_leader.py
@@ -26,6 +26,7 @@ def find_leader(array):
             print('Brak lidera w zbiorze')
             return
 
+
 if __name__ == "__main__":
     tab = [1, 2, 3, 4, 1, 1, 1, 1, 6, 7, 1, 9, 1]
     find_leader(tab)
