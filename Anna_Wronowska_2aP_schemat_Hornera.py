00dfb67fefd8346621707e5975bcf55719c11deb
AnnaWronowska <103896687+AnnaWronowska@users.noreply.github.com>
2022-04-19 19:32:11 +0200

    Update Anna_Wronowska_2aP_schemat_Hornera.py

--- Anna_Wronowska_2aP_schemat_Hornera.py
+++ Anna_Wronowska_2aP_schemat_Hornera.py
@@ -1,21 +1,22 @@
-def horner_ite(n, x, list1):
+def horner_ite(n: int, x, list1: list):
     list2 = [list1[0]]
     for i in range(1, n + 1):
         list2.append(list2[i - 1] * x + list1[i])
     return list2[n]
 
 
-def horner_rec(n, x, list1):
+def horner_rec(n: int, x, list1: list):
     if n == 0:
         return list1[0]
     return horner_rec(n - 1, x, list1) * x + list1[n]
 
 
-n0 = int(input('Podaj stopien wielomianu\n'))
-list0 = []
-print('Podaj kolejne wspolczynniki wielomianu')
-for i in range(n0 + 1):
-    list0.append(int(input()))
-x0 = int(input('Podaj wartosc x\n'))
-print('Horner iteracyjnie: ' + f'{horner_ite(n0, x0, list0)}')
-print('Horner rekurencyjnie: ' + f'{horner_rec(n0, x0, list0)}')
+if __name__ == "__main__":
+    n0 = int(input('Podaj stopien wielomianu\n'))
+    list0 = []
+    print('Podaj kolejne wspolczynniki wielomianu')
+    for i in range(n0 + 1):
+        list0.append(int(input()))
+    x0 = int(input('Podaj wartosc x\n'))
+    print('Horner iteracyjnie: ' + f'{horner_ite(n0, x0, list0)}')
+    print('Horner rekurencyjnie: ' + f'{horner_rec(n0, x0, list0)}')
